b0962f054a11e1917c47d1fe3db5a74cd6333267
Fernando Venâncio <fervenan7@gmail.com>
2024-04-17 17:27:29 -0300

    Desafio Concluído

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,15 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // Implementado
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new ArgumentException("A capacidade excedeu o número de hóspedes. Não é possível fazer a reserva");// TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                // Implementado
             }
         }
 
@@ -35,26 +34,24 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // Implementado
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // Implementado
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // Implementado
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                decimal desconto = valor * 0.1m;
+                valor = valor - desconto;
             }
 
             return valor;
+            
         }
     }
 }
